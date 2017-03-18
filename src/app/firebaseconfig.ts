3d55a90cd260fb8dc1313326ab4c416564e8b8e2
Jan Van Braeckel <JanVanBraeckel@users.noreply.github.com>
2017-03-18 11:42:46 +0100

    Create firebaseconfig.ts

---
+++ src/app/firebaseconfig.ts
@@ -0,0 +1,7 @@
+export const fireBaseConfig = {
+  apiKey: '$FIREBASE_APIKEY',
+  authDomain: '$FIREBASE_AUTHDOMAIN',
+  databaseURL: '$FIREBASE_DATABASEURL',
+  storageBucket: '$FIREBASE_STORAGEBUCKET',
+  messagingSenderId: '$FIREBASE_MESSAGESENDERID'
+};
